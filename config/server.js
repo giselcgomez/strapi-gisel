439c4c06a1151e152ad5b222cb042139b2df1455
giselcgomez <me@giselgomez.com>
2022-10-07 19:55:01 -0600

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b9f59f173dfc1def56a17d6fe419a069'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '24cd7a99-d6e8-454e-b67d-276843fec1da'
+};
